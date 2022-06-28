ec44c4a965f91af7bcd4e14d119d9a7ac74750bd
agiachris <christopheragia@gmail.com>
2022-06-27 18:18:16 -0700

    add MIT license

--- setup.py
+++ setup.py
@@ -16,12 +16,13 @@ setuptools.setup(
     version="0.1.0",
     author="Apoorva Sharma, Christopher Agia",
     author_email="apoorva@stanford.edu, cagia@cs.stanford.edu",
-    description="Equip PyTorch models with SCOD for OoD detection",
+    description="Equip PyTorch models with SCOD for OoD detection on regression tasks",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/agiachris/scod-module",
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
     ],
     packages=setuptools.find_packages(),
